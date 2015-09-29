44123fa24811255800c92aebde86533b3d300cd4
Chris Loew <cloew123@gmail.com>
2015-09-29 16:48:39 -0600

    Added basic Kao Checkbox Button

---
+++ dist/kao_checkbox_button.js
@@ -0,0 +1,35 @@
+$traceurRuntime.ModuleStore.getAnonymousModule(function() {
+  "use strict";
+  angular.module("kao.checkbox-button", []).directive("checkboxButton", function() {
+    return {
+      restrict: "E",
+      replace: true,
+      transclude: true,
+      scope: {
+        selected: "=",
+        onToggle: "=",
+        onToggleOn: "=",
+        onToggleOff: "="
+      },
+      controller: function($scope) {
+        $scope.toggle = function() {
+          $scope.selected = !$scope.selected;
+          if ($scope.onToggle) {
+            $scope.onToggle($scope.selected);
+          }
+          if (!!$scope.selected && !!$scope.onToggleOn) {
+            $scope.onToggleOn();
+          } else {
+            if (!$scope.selected && !!$scope.onToggleOff) {
+              $scope.onToggleOff();
+            }
+          }
+        };
+      },
+      template: "<button class=\"btn btn-primary\" ng-click=\"toggle()\">     <div class=\"text-left\">         <span class=\"glyphicon glyphicon-check\" ng-show=\"selected\"></span>         <span class=\"glyphicon glyphicon-unchecked\" ng-hide=\"selected\"></span>         <ng-transclude style=\"margin-left: 5px;\"></ng-transclude>     </div> </button>"
+    };
+  });
+  return {};
+});
+
+//# sourceMappingURL=kao_checkbox_button.map
